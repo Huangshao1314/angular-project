bc3d590f0fef7f609202f28f315d2957513312d0
HuangShao1314 <1052912975@qq.com>
2019-01-31 21:46:04 +0800

    angular ng-zorro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,76 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
-import { PcontentComponent } from './components/pcontent/pcontent.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
 
+import { HomeComponent } from './home/home.component';
+
+
+import { UserlistComponent } from './user/userlist/userlist.component';
+import { VipComponent } from './user/vip/vip.component';
+import { AdduserComponent } from './user/adduser/adduser.component';
+import { EdituserComponent } from './user/edituser/edituser.component'
+
+//配置路由
 const routes: Routes = [
+  {
+    path: 'home',
+    component:HomeComponent,
+   
+  },
+   {
+    path: 'shop',
+    component:ShopComponent 
 
-  {path:'home',component:HomeComponent},
+  },
+  {
+    path: 'product',
+    component:ProductComponent 
 
-  {path:'pcontent/:id',component:PcontentComponent},
+  },
+  {
+    path: 'user',
+    component:UserComponent,
+     children:[   /*配置子路由*/
+      {
+        path: 'list',
+        component:UserlistComponent 
+    
+      },
+      {
+        path: 'vip',
+        component:VipComponent 
+    
+      }
+      ,
+      {
+        path: 'add',
+        component:AdduserComponent 
+    
+      }
+      ,
+      {
+        path: 'edit',
+        component:EdituserComponent 
+    
+      },{   
+        path: '**',  /*任意的路由*/
+        // component:HomeComponent
+        redirectTo:'list'
+      }
+      
+    ]
 
-  {path:'**',redirectTo:'home'}
+  }
+  ,{   /*匹配不到路由的时候加载的组件*/
+    path: '**',  /*任意的路由*/
+    // component:HomeComponent
+    redirectTo:'home'
+  }
+
+  
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularproject';
+export class AppComponent implements OnInit {
+  title = 'app';
+  isCollapsed=false;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
